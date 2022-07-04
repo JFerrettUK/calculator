4e81dbf1dc8ce6dcb4f88331a892669a75041662
JFerrettUK <jferrettuk@gmail.com>
2022-07-04 09:27:20 +0100

    Updates to Calculator JS

--- js-file.js
+++ js-file.js
@@ -1,19 +1,25 @@
-function calcAdd(input1, input2) {
-    addResult = input1 + input2;
-    return addResult;
-}
-
-function calcMin(input1, input2) {
-    minResult = input1 - input2;
-    return minResult;
-}
-
-function calcTimes(input1, input2) {
-    timesResult = input1 * input2;
-    return timesResult;
-}
-
-function calcDivide(input1, input2) {
-    divideResult = input1 / input2;
-    return divideResult;
+function operate(input1, operator, input2) {
+    let result = 0;
+    if (operator == "+") {
+        function add(input1, input2) {
+            result = input1 + input2;
+            return result;
+        }
+    } else if (operator == "-") {
+        function minus(input1, input2) {
+            result = input1 - input2;
+            return result;
+        }
+    } else if (operator == "*") {
+        function times(input1, input2) {
+            result = input1 * input2;
+            return result;
+        }
+    } else if (operator == "%") {
+        function divide(input1, input2) {
+            result = input1 / input2;
+            return result;
+        }
+    return result;
+    }
 }
