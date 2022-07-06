0048a73f265173d45218c736b5a7c516cbd78cae
JFerrettUK <jferrettuk@gmail.com>
2022-07-06 09:18:33 +0100

    Calculator addEventListener tested and placed

--- js-file.js
+++ js-file.js
@@ -35,3 +35,62 @@ function operate(input1, operator, input2) {
         return result;
     }
 }
+
+document.getElementById("1").addEventListener("click", function(){
+    ani(1);
+});
+document.getElementById("2").addEventListener("click", function(){
+    ani(2);
+});
+document.getElementById("3").addEventListener("click", function(){
+    ani(3);
+});
+document.getElementById("4").addEventListener("click", function(){
+    ani(4);
+});
+document.getElementById("5").addEventListener("click", function(){
+    ani(5);
+});
+document.getElementById("6").addEventListener("click", function(){
+    ani(6);
+});
+document.getElementById("7").addEventListener("click", function(){
+    ani(7);
+});
+document.getElementById("8").addEventListener("click", function(){
+    ani(8);
+});
+document.getElementById("9").addEventListener("click", function(){
+    ani(9);
+});
+document.getElementById("0").addEventListener("click", function(){
+    ani(0);
+});
+document.getElementById('minus').addEventListener("click", function(){
+    ani('minus');
+});
+document.getElementById('add').addEventListener("click", function(){
+    ani('add');
+});
+document.getElementById('divide').addEventListener("click", function(){
+    ani('divide');
+});
+document.getElementById('times').addEventListener("click", function(){
+    ani('times');
+});
+document.getElementById('clear').addEventListener("click", function(){
+    ani('clear');
+});
+document.getElementById('equals').addEventListener("click", function(){
+    ani('equals');
+});
+
+function ani(element) {
+    let change = document.getElementById(element);
+    setTimeout(function() {
+        change.style.display = (change.style.background = 'darkgrey');
+         setTimeout(function() {
+            change.style.display = (change.style.background = 'lightgrey');
+         }, 100);
+    }, 100);
+}
